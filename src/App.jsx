aedbec8b9123e092fe4c3eae8d2c4f48fdc1ff3e
mohamad sajadi <mohamadcodew84@gmail.com>
2025-05-20 15:20:19 +0330

    create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css'
+import HomePage from './components/template/HomePage';
 
 function App() {
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    'x-cg-demo-api-key': 'CG-V7WPnLJE4UGyxfsEqVz1keSV	'
+  }
+};
 
+fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=20&page=1', options)
+  .then(res => res.json())
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
   return (
     <>
-  <h1 className='bg-amber-500'>crypto</h1>
+<div>
+  <HomePage/>
+</div>
       
     </>
   )
