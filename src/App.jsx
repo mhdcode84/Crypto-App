6df6cf9182038834dc3adf4cf6a326c54abdbcea
mohamad sajadi <mohamadcodew84@gmail.com>
2025-05-21 16:52:21 +0330

    show coin in table

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,6 @@ import './App.css'
 import HomePage from './components/template/HomePage';
 
 function App() {
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    'x-cg-demo-api-key': 'CG-V7WPnLJE4UGyxfsEqVz1keSV	'
-  }
-};
-
-fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=20&page=1', options)
-  .then(res => res.json())
-  .then(res => console.log(res))
-  .catch(err => console.error(err));
   return (
     <>
 <div>
--- src/components/template/HomePage.jsx
+++ src/components/template/HomePage.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import TableCoin from '../modules/TableCoin'
+import { getCoinList } from '../../services/CryptoApi';
 
 function HomePage() {
+  const [coins, setcoins] = useState([])
+  useEffect(() => {
+    const getData = async () => {
+      const fetchData = await fetch(getCoinList())
+      const json = await fetchData.json()
+      setcoins(json)
+    }
+    getData()
+  }, [])
   return (
-    <div>HomePage</div>
+    <div>
+      <TableCoin coins={coins} />
+    </div>
   )
 }
 
