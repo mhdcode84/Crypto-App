f971b2b4ff57de6f8045f6d138995a8778821775
mohamad sajadi <mohamadcodew84@gmail.com>
2025-05-28 08:59:32 +0330

    add Loader spinner & add &  destructure Table Row

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -1,8 +1,10 @@
 import Chartup from "../../assets/chart-up.svg"
 import Chartdown from "../../assets/chart-down.svg"
-function TableCoin({ coins }) {
+import { RingLoader } from "react-spinners";
+function TableCoin({ coins , Isloading}) {
     return (
         <div>
+         {Isloading?<p><RingLoader color="#0004ff" /></p>:
             <table>
                 <thead>
                     <tr>
@@ -16,24 +18,34 @@ function TableCoin({ coins }) {
                 </thead>
                 <tbody>
                     {coins.map((coin) => (
-                        <tr key={coin.id}>
-                            <td>
-                                <div>
-                                    <img src={coin.image} alt="" />
-                                    <span>{coin.symbol.toUpperCase()}</span>
-                                </div>
-                            </td>
-                            <td>{coin.name}</td>
-                            <td>${coin.current_price.toLocaleString()}</td>
-                            <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
-                            <td>{coin.total_volume.toLocaleString()}</td>
-                            <td><img src={coin.price_change_percentage_24h > 0 ? Chartup : Chartdown} alt={coin.name} /></td>
-                        </tr>
+                        <TableRow coin={coin} key={coin.id} />
                     ))}
                 </tbody>
-            </table>
+            </table>}
         </div>
     )
 }
 
 export default TableCoin;
+const TableRow = (
+    { coin:{
+        name, image,
+        current_price,
+        total_volume,
+        price_change_percentage_24h: price_change, symbol,
+        id,}
+    }) => (
+    <tr key={id}>
+        <td>
+            <div>
+                <img src={image} alt="" />
+                <span>{symbol.toUpperCase()}</span>
+            </div>
+        </td>
+        <td>{name}</td>
+        <td>${current_price.toLocaleString()}</td>
+        <td>{price_change.toFixed(2)}%</td>
+        <td>{total_volume.toLocaleString()}</td>
+        <td><img src={price_change > 0 ? Chartup : Chartdown} alt={name} /></td>
+    </tr>
+)
--- src/components/template/HomePage.jsx
+++ src/components/template/HomePage.jsx
@@ -4,17 +4,19 @@ import { getCoinList } from '../../services/CryptoApi';
 
 function HomePage() {
   const [coins, setcoins] = useState([])
+  const [Isloading,Setisloading] = useState(true)
   useEffect(() => {
     const getData = async () => {
       const fetchData = await fetch(getCoinList())
       const json = await fetchData.json()
       setcoins(json)
+      Setisloading(false)
     }
     getData()
   }, [])
   return (
     <div>
-      <TableCoin coins={coins} />
+      <TableCoin coins={coins} Isloading={Isloading} />
     </div>
   )
 }
